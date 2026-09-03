06646f246d378e2d0ebca3737fa86185e9e4dbc2
jw <jwgellard@gmail.com>
2026-09-03 16:12:24 +0800

    Bootstrap workspace with stub crates

---
+++ crates/crpg-godot/src/lib.rs
@@ -0,0 +1,17 @@
+#![allow(unsafe_code)]
+#![warn(missing_docs)]
+//! TODO: crate purpose.
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
